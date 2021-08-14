29663bc0f64a67ef51520fad903b1388d00ee790
Lucas Kjaero <lucas@lucaskjaero.com>
2021-08-15 00:02:48 +0200

    Actually bring in dependencies

--- build.sbt
+++ build.sbt
@@ -1,12 +1,23 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.12.12"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "io.fluentlabs"
+ThisBuild / scalaVersion := "2.12.12"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "io.fluentlabs"
 ThisBuild / organizationName := "fluentlabs"
 
 lazy val root = (project in file("."))
   .settings(
     name := "jobs",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies += Seq(
+      Dependencies.scalaTest % Test,
+      Dependencies.content,
+      Dependencies.sparkCore % "provided",
+      Dependencies.sparkSql % "provided",
+      Dependencies.sparkXml,
+      // S3 support
+      Dependencies.hadoop,
+      Dependencies.hadoopClient,
+      Dependencies.hadoopAWS,
+      Dependencies.awsJavaSDK
+    )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,22 @@
 import sbt._
 
 object Dependencies {
+  val hadoopVersion = "3.3.1"
+  val sparkVersion = "3.1.2"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
+  val content = "io.fluentlabs" %% "content" % "1.0.1"
+  val sparkCore =
+    "org.apache.spark" %% "spark-core" % sparkVersion
+  val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion
+  val sparkXml = "com.databricks" %% "spark-xml" % "0.12.0"
+
+  val hadoop = "org.apache.hadoop" % "hadoop-common" % hadoopVersion
+  val hadoopClient = "org.apache.hadoop" % "hadoop-client" % hadoopVersion
+  val hadoopAWS = "org.apache.hadoop" % "hadoop-aws" % hadoopVersion
+  val awsJavaSDK = "com.amazonaws" % "aws-java-sdk" % "1.12.42"
+  // Enable this when it is build for scala 2.12 in main
+  // And remove our local version
+  //    val elasticsearchHadoop =
+  //      "org.elasticsearch" % "elasticsearch-hadoop" % elasticsearchVersion
 }
