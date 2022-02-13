7e161f4eb4707251cbe3f664f688b2ea77cda256
Lucas Kjaero-Zhang <lucas@lucaskjaero.com>
2022-02-13 00:19:49 +0000

    Update content to 1.0.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val sparkVersion = "3.2.1"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
-  val content = "io.fluentlabs" %% "content" % "1.0.17"
+  val content = "io.fluentlabs" %% "content" % "1.0.20"
 
   val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion
