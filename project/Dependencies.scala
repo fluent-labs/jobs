9b32f544cec3725a1e85f6d74586f716d7f28a9e
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2022-07-10 10:23:17 -0700

    Update spark-core, spark-sql to 3.3.0 (#114)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val hadoopVersion = "3.3.1"
   val jacksonVersion = "2.11.3"
-  val sparkVersion = "3.2.1"
+  val sparkVersion = "3.3.0"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
   val content = "io.fluentlabs" %% "content" % "1.0.17"
