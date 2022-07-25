c5b464cb2e347705143452d875f51714e2792a1e
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-26 01:53:10 +0200

    Language specific wiktionary jobs

---
+++ src/main/scala/io/fluentlabs/jobs/definitions/analyze/wiktionary/section/DanishWiktionarySectionFinder.scala
@@ -0,0 +1,4 @@
+package io.fluentlabs.jobs.definitions.analyze.wiktionary.section
+
+object DanishWiktionarySectionFinder
+    extends WiktionarySectionFinder("dawiktionary") {}
---
+++ src/main/scala/io/fluentlabs/jobs/definitions/analyze/wiktionary/section/SimpleWiktionarySectionFinder.scala
@@ -0,0 +1,4 @@
+package io.fluentlabs.jobs.definitions.analyze.wiktionary.section
+
+object SimpleWiktionarySectionFinder
+    extends WiktionarySectionFinder("simplewiktionary") {}
