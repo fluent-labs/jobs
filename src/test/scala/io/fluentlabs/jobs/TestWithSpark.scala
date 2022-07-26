ab76289ef3858a468118e805172470bd89994124
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-26 10:46:00 +0200

    Share the spark session with all tests

--- src/test/scala/io/fluentlabs/jobs/TestWithSpark.scala
+++ src/test/scala/io/fluentlabs/jobs/TestWithSpark.scala
@@ -2,14 +2,18 @@ package io.fluentlabs.jobs
 
 import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
 
-trait TestWithSpark {
-  implicit lazy val spark: SparkSession = {
+object SharedSparkSession {
+  val spark: SparkSession = {
     SparkSession
       .builder()
       .master("local")
-      .appName("spark test example")
+      .appName("Spark test runner")
       .getOrCreate()
   }
+}
+
+trait TestWithSpark {
+  implicit lazy val spark: SparkSession = SharedSparkSession.spark
 
   def createDataset[T: Encoder](data: Seq[T]): Dataset[T] = {
     spark.createDataset(data)
