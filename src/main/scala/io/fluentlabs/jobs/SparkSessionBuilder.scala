980b4c3c671b2954c88d4d04683576bf7b5db7c1
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-26 23:02:04 +0200

    Set up digitalocean settings

--- src/main/scala/io/fluentlabs/jobs/SparkSessionBuilder.scala
+++ src/main/scala/io/fluentlabs/jobs/SparkSessionBuilder.scala
@@ -5,9 +5,15 @@ import org.apache.spark.sql.SparkSession
 // $COVERAGE-OFF$
 object SparkSessionBuilder {
   def build(name: String): SparkSession = {
+    val s3Endpoint =
+      sys.env.getOrElse("s3Endpoint", "https://fra1.digitaloceanspaces.com")
+
     SparkSession
       .builder()
       .appName(name)
+      .config("fs.s3a.access.key", sys.env("AWS_ACCESS_KEY_ID"))
+      .config("fs.s3a.secret.key", sys.env("AWS_SECRET_ACCESS_KEY"))
+      .config("fs.s3a.endpoint", s3Endpoint)
       .getOrCreate()
   }
 }
